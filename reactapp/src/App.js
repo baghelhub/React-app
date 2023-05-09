694ef6681831802497bd8d75853c8f95a1f98a2d
amit <amitbaghel202@gmail.com>
2023-05-09 14:56:49 +0530

    new component add

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,15 +1,14 @@
 
 import './App.css';
-
+import Navbar from './components/navbar';
 
 function App() {
-  return (
-  <>
-  
-</>
 
-  );
-}
-
+       return (
+         <>
+         < Navbar />
+         </>
+      );
+  }
 
 export default App;
