b0d8563223498c579f920cf45401fb713258b8a2
amit <amitbaghel202@gmail.com>
2023-05-10 15:45:16 +0530

    add new darkmode file and set color

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,19 +1,17 @@
 
 import './App.css';
+// import About from './components/About';
 import Textform from './components/Textform';
 // new file import for use usestate hook
 //import SetCount from './components/SetCount';
-
-
 
 function App() {
 
   return (
     <>
-      < Textform head="Enter The Text Analyze  " nextText="UseState Hook" />
-
-
+      < Textform head="Enter The Text Analyze" nextText="UseState Hook" />
       {/* < SetCount  /> */}
+      {/* <About /> */}
     </>
   );
 }
