ef586d3259105c23fceadaebdcf6df5f18a2d39b
amit <amitbaghel202@gmail.com>
2023-05-10 16:31:22 +0530

    new changes

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
- //import About from './components/About';
+//import About from './components/About';
 import Textform from './components/Textform';
 // new file import for use usestate hook
 //import SetCount from './components/SetCount';
@@ -9,9 +9,9 @@ function App() {
 
   return (
     <>
-    < Textform head="Enter The Text Analyze" nextText="UseState Hook" />
+      < Textform head="Enter The Text Analyze" nextText="UseState Hook" />
       {/* < SetCount  /> */}
-{/* <About /> */}
+      {/* <About /> */}
     </>
   );
 }
