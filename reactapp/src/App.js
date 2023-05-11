85c144abe21a410b856598f6c9e518c75978ea68
amit <amitbaghel202@gmail.com>
2023-05-11 19:10:37 +0530

    add files for routing and use it

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,12 +1,17 @@
 
-import { React} from 'react';
+import { React } from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Help from './components/Help';
+
 // import About from './components/About';
-import Textform from './components/Textform';
-import Navbar from './components/Navbar';
+// import Textform from './components/Textform';
+ import Navbar from './components/Navbar';
 
 // new file import for use usestate hook
 //import SetCount from './components/SetCount';
+
 
 function App() {
 
@@ -14,7 +19,14 @@ function App() {
   return (
     <>
       <Navbar />
-      < Textform head="Enter The Text Analyze" nextText="UseState Hook" />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/Help" element={<Help />}></Route>
+        </Routes>
+      </BrowserRouter>
+
+      {/* < Textform head="Enter The Text Analyze" nextText="UseState Hook" /> */}
       {/* < SetCount  /> */}
       {/* <About /> */}
     </>
