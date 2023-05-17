94090aa8a2209e0f923f1ca8d5ed7bfed6682be0
amit <amitbaghel202@gmail.com>
2023-05-17 16:01:23 +0530

    countory file updated

--- reactapp/src/components/countrySelect.js
+++ reactapp/src/components/countrySelect.js
@@ -2,11 +2,13 @@ import { React, useState } from 'react'
 
 export default function CountrySelect() {
 
+    /* collect of array object for countory state and city */
+
     const Countories = [
         {
             name: "India",
             stata: [
-                { name: "Dehli", cities: ["iarea 1", "iarea 2"] },
+                { name: "Dehli", cities: ["iarea 1", "iarea 2","iarea 3"] },
                 { name: "Up", cities: ["iarea 3", "iarea 4"] },
                 { name: "Haryana", cities: ["iarea 5", "iarea 6"] }
             ]
@@ -31,46 +33,48 @@ export default function CountrySelect() {
 
     const chnagecountry = (e) => {
 
-        setCountry(e.target.value);
+        setCountry(e.target.value)
         setStates(Countories.find(ctr => ctr.name === e.target.value).stata)
     }
 
-    const chnageState = (e) => {
-
-        setState(e.target.value);
-        setCities(stata.find(sta => sta.name === e.target.value).cities)
+    const chnageState = (e) =>{
+        setState(e.target.value)
+        setCities(stata.find(sta=> sta.name === e.target.value).cities)
     }
-
+    const chnageCity = (e) =>{
+        setCity(e.target.value)
+      }
     return (
         <>
             <div className='container col-md-6 mt-4'>
                 <h4>Select Country State and City</h4>
                 <select className="form-select form-select-lg mb-3 mt-3" onChange={chnagecountry} aria-label=".form-select-lg example" >
-                    <option selected>--select Country--</option>
+                    <option selected>{country}</option>
                     {Countories.map((ctr) => (
                         <option value={ctr.name}>{ctr.name}</option>
                     )
                     )}
                 </select>
-                <select className="form-select form-select-sm mb-3" onChange={chnageState} aria-label=".form-select-sm example">
-                    <option selected>--select state--</option>
+                <select className="form-select form-select-sm mb-3" onChange={chnageState}  aria-label=".form-select-sm example">
+                    <option selected>{state}</option>
 
                     {stata.map((sta) => (
                         <option value={sta.name}>{sta.name}</option>
 
                     ))}
                 </select>
-                <select className="form-select form-select-sm mb-2" aria-label=".form-select-sm example">
+                <select className="form-select form-select-sm mb-2" onChange={chnageCity}  aria-label=".form-select-sm example">
+                    
                     <option selected>{city}</option>
 
-                    {cities.map(city => {
+                    {cities.map(city => (
 
                         <option value={city}>{city}</option>
-                       
-                    })}
+
+                    ))}
 
                 </select>
-                <button type="button" className="btn btn-secondary">Secondary</button>
+                <button type="button" className="btn btn-primary btn-xs">Save</button>
             </div>
         </>
     )
